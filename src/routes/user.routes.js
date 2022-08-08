8a0cecd995c7fc64293b3acd3e5493bcb20fd745
mateus <fmfr@outlook.com>
2022-08-08 16:37:27 -0300

    add middleware example (not gonna use this)

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,9 +4,30 @@ const UsersController = require("../controllers/UsersController")
 
 const usersRoutes = Router();
 
+function myMiddleware(request, response, next) {
+    console.log("Você passou pelo Middleware!");
+
+    if(!request.body.isAdmin){
+        return response.json({ message:"user unauthorized" });
+    }
+
+    //como ele sabe que essa função next()
+    //chama a userController.create ??
+    //pois essa função não foi declarada
+    //em nenhum local
+    next();
+}
+    
 const userController = new UsersController()
 
-
+//aplicando Middleware para todas as rotas de uma vez
+usersRoutes.use(myMiddleware)
 usersRoutes.post("/", userController.create);
+// DE NOVO ESSA PORRA DE use() aaaA
+
+
+//aplicando Middleware rota por rota
+//usersRoutes.post("/", myMiddleware, userController.create);
+
 
 module.exports = usersRoutes;
